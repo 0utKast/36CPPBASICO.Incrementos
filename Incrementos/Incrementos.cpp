466debe8917434ee9ea2fd4892efe3783375ae2d
Jesus Conde <educaleft@gmail.com>
2021-12-02 03:23:40 +0000

    Incluido código operador decremento

--- Incrementos/Incrementos.cpp
+++ Incrementos/Incrementos.cpp
@@ -1,19 +1,27 @@
-// Incrementos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x{ 8 };
+    int y = x++; // x es incrementado a 9, copia del x original es evaluado al valor 8, y 8 es asignado a y
+
+    std::cout << x << ' ' << y;
+    return 0;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+
+
+
+
+
+/*#include <iostream>
+
+int main()
+{
+    int x{ 8 };
+    int y = ++x; // x es incrementado a 9, x es evaluado al valor 9, y 9 es asignado a y
+
+    std::cout << x << ' ' << y;
+    return 0;
+}*/
