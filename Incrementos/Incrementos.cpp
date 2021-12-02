17233a601b529d4a36500ce76d0f300107f9581b
Jesus Conde <educaleft@gmail.com>
2021-12-02 03:29:28 +0000

    Nuevo ejemplo de uso de incremento y decremento prefijo y postfijo

--- Incrementos/Incrementos.cpp
+++ Incrementos/Incrementos.cpp
@@ -2,12 +2,39 @@
 
 int main()
 {
+    int x{ 5 };
+    int y{ 5 };
+    std::cout << x << ' ' << y << '\n';
+    std::cout << ++x << ' ' << --y << '\n'; // prefijo
+    std::cout << x << ' ' << y << '\n';
+    std::cout << x++ << ' ' << y-- << '\n'; // postfijo
+    std::cout << x << ' ' << y << '\n';
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+
+int main()
+{
     int x{ 8 };
     int y = x++; // x es incrementado a 9, copia del x original es evaluado al valor 8, y 8 es asignado a y
 
     std::cout << x << ' ' << y;
     return 0;
-}
+}*/
 
 
 
